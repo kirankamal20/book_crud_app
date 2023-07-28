4cf7b246bcb46f5cf8103a8b923f84ffd3e6d360
kirankamal <kiran.k@lucidplus.com>
2023-07-28 20:45:24 +0530

    refactor : theme updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:loginapp/login/presentation/login_page.dart';
 
 void main() {
-  
+  runApp(const MyApp());
+}
 
-  runApp(const MaterialApp(
-    home: LoginPage(),
-  ));
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(
+              color: Colors.redAccent,
+              iconTheme: IconThemeData(color: Colors.white)),
+          inputDecorationTheme:
+              const InputDecorationTheme(contentPadding: EdgeInsets.all(15))),
+      home: const LoginPage(),
+    );
+  }
 }
